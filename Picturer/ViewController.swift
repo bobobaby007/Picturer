fadfe83eb3bfa3b81983d401c76fe6a793568dee
bobobaby007 <bobobaby007@hotmail.com>
2015-06-24 00:28:00 +0800

    Initial Commit

---
+++ Picturer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picturer
+//
+//  Created by Bob Huang on 15/6/24.
+//  Copyright (c) 2015年 4view. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
